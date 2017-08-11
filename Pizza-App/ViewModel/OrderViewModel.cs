d0027594085a2973c2618bde3e4d647ed8161dc7
Chris Tran <christopher.v.tran@gmail.com>
2017-08-11 13:28:40 -0500

    added views for home page and order history

---
+++ Pizza-App/ViewModel/OrderViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Pizza_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pizza_App.ViewModel
+{
+    public class OrderViewModel
+    {
+        public CustomerOrder CustomerOrder { get; set; }
+        public IEnumerable<PizzaType> PizzaType { get; set; }
+    }
+}
